f96a5836efa18fea3639c4e68ad51ebb1a34d078
OvalQuilter <69106951+OvalQuilter@users.noreply.github.com>
2022-01-10 03:23:00 +0200

    1.0.0 Update

--- LyricsSender.js
+++ LyricsSender.js
@@ -9,14 +9,16 @@ var playbackStateUpdater = null,
 
 const menuUI = $("<div/>", {
           id: "menuUI",
-          class: "hidden",
+          class: "hid",
       }).css({
           width: "200px",
           height: "200px",
           backgroundColor: "rgba(100, 100, 100, 0.7)",
+          borderRadius: "5px 0 0 5px",
           left: "100%",
           top: "55%",
           transform: "translate(-100%, -50%)",
+          transition: "transform 0.5s cubic-bezier(0.68, -0.55, 0.27, 1.55)",
           position: "fixed",
           zIndex: "9999"
       }),
@@ -60,6 +62,15 @@ const menuUI = $("<div/>", {
           borderRadius: "2px",
           backgroundColor: "rgba(110, 110, 110, 0.6)"
       }).appendTo(menuUI),
+      version = $("<span/>", {
+          id: "version"
+      }).css({
+          position: "absolute",
+          left: "50%",
+          bottom: "0",
+          transform: "translate(-50%)",
+          fontSize: "10px"
+      }).html("Version 1.0.0").appendTo(menuUI),
       style = $("<style/>").html(`
       .btn {
           position: relative;
@@ -108,6 +119,20 @@ const menuUI = $("<div/>", {
       #menuUI button {
           cursor: pointer;
       }
+      #logWindow::-webkit-scrollbar {
+          width: 5px;
+          height: 5px;
+      }
+      #logWindow::-webkit-scrollbar-thumb {
+          background-color: rgba(75, 75, 75, 0.6);
+          border-radius: 2px;
+      }
+      #logWindow::-webkit-scrollbar-thumb:hover {
+         background-color: rgba(65, 65, 65, 0.6);
+      }
+      #logWindow::-webkit-scrollbar-thumb:active {
+         background-color: rgba(60, 60, 60, 0.6);
+      }
       #logWindow span {
           margin: 1px 0 0 2px;
           float: left;
@@ -121,10 +146,13 @@ const menuUI = $("<div/>", {
       .log {
           color: rgba(150, 150, 200, 0.6);
       }
+      .hid {
+          transform: none !important;
+      }
       #tokenInput::placeholder {
           font-size: 12px;
       }
-      `).appendTo(document.body);
+      `).appendTo(menuUI);
 
 // UI //
 
@@ -134,13 +162,15 @@ const menuUI = $("<div/>", {
 
 $(document).keyup(function(e) {
     switch (e.key) {
-        case "Escape": $(menuUI).toggleClass("hidden");
+        case "Escape": $(menuUI).toggleClass("hid");
             break;
     }
 });
 
-$(startButton).click(()=>{if(!stopped){return;}stopped=false;init()});
-$(stopButton).click(()=>{if(stopped){return;}stop()});
+logWindow.hover(()=>{logWindow.css({overflow:"hidden auto"});},()=>{logWindow.css({overflow:"hidden"});});
+
+startButton.click(()=>{if(!stopped){return;}stopped=false;init()});
+stopButton.click(()=>{if(stopped){return;}stop()});
 
 // Events //
 
@@ -152,48 +182,44 @@ $(tokenInput).val(localStorage.getItem("discordToken") ?? "");
 // If exists, restore the previous Discord token we've had
 
 function addLog(text, t) {
-    let type = t == "error" ? "Error" : t == "warning" ? "Warning" : "Log";
-    $("<span/>", { class: type.toLowerCase() }).html(`[${type}]: ${text}`).appendTo(logWindow)[0].scrollIntoView(false);
+    t = t == "error" ? "Error" : t == "warning" ? "Warning" : "Log";
+    $("<span/>", { class: t.toLowerCase() }).html(`[${t}]: ${text}`).appendTo(logWindow)[0].scrollIntoView(false);
 }
 
 function formatSeconds(s){return(s-(s%=60))/60+(9<s?':':':0')+s}
 
-async function refreshAccessToken() {
-    let response = await fetch("https://open.spotify.com/get_access_token?reason=transport&productType=web_player", { credentials: "same-origin" });
-    let json = await response.json();
-    accessToken = json.accessToken;
+function refreshAccessToken() {
+    $.get({ url: "https://open.spotify.com/get_access_token?reason=transport&productType=web_player", async: false, success: (d) => accessToken = d.accessToken});
 }
 
-function changeStatus(text, emojiID, emojiName) {
+function changeStatus(text, emojiID = null, emojiName = null) {
     const start = Date.now();
-        const token = $(tokenInput).val();
+    const token = $(tokenInput).val();
 
-        var xhr = new XMLHttpRequest();
-        xhr.open("PATCH", "https://discordapp.com/api/v8/users/@me/settings");
-
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.setRequestHeader("Authorization", token);
-
-        xhr.onreadystatechange = function() {
-            if(xhr.readyState === 4) {
-
-                if(xhr.status === 401) {
-                    addLog("You provided invalid token!", "error");
-                    return stop();
-                }
-                // If invalid token provided
-
-            }
-        };
-
-        xhr.send(JSON.stringify({
-				  "custom_status": {
+    $.ajax({
+        url: "https://discordapp.com/api/v8/users/@me/settings",
+        method: "PATCH",
+        dataType: "json",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": token
+        },
+        data: JSON.stringify({
+				"custom_status": {
 				    "text": text,
-					  "emoji_id": emojiID,
-					  "emoji_name": emojiName,
-            "expires_at": null
-				  }
-        }));
+					"emoji_id": emojiID,
+					"emoji_name": emojiName,
+                    "expires_at": null
+				}
+              }),
+        statusCode: {
+            200: async () => console.log("Status change request took: " + (Date.now() - start) + "ms"),
+            401: async (x, e) => {
+                addLog("You provided invalid token!", "error");
+                return stop();
+            }
+        }
+    });
 }
 
 function init() {
@@ -208,72 +234,80 @@ function init() {
     localStorage.setItem("discordToken", $(tokenInput).val());
 
     playbackStateUpdater = setInterval(function() {
-        let getPlaybackState = new XMLHttpRequest();
+        let getPlaybackState = $.ajax({
+            url: "https://api.spotify.com/v1/me/player/currently-playing?market=ES",
+            method: "GET",
+            dataType: "json",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${accessToken}`
+            },
+            statusCode: {
+                200: async (d) => {
+                    if(lastTrackId !== d.item.id) {
+                        lastTrackId = d.item.id
+                        trackDurationMs = d.item.duration_ms;
 
-        getPlaybackState.open("GET", "https://api.spotify.com/v1/me/player/currently-playing?market=ES");
+                        lyrics = [];
 
-        getPlaybackState.setRequestHeader("Content-Type", "application/json");
-        getPlaybackState.setRequestHeader("Authorization", `Bearer ${accessToken}`);
-
-        getPlaybackState.onreadystatechange = async function() {
-            if(getPlaybackState.readyState === 4) {
-		    
-		    console.log(getPlaybackState.response);
-
-                switch(getPlaybackState.status) {
-                    case 204:
-                        addLog("You\'re not listening music!", "error");
-                        return stop();
-                        break;
-                    case 401:
-                        await refreshAccessToken();
-                        addLog("Refreshed access token due to expired previous one. <strong>Keep working...</strong>", "warning");
-                        return;
-                        break;
-                    case 502:
-                        addLog("Got 502 error, might be problem with Spotify itself. If it continues for a time, try refreshing the page or wait few minutes.", "error");
-                        return;
-                        break;
-                }
-
-                let response = JSON.parse(getPlaybackState.response);
-
-                if(lastTrackId !== response.item.id) {
-                    trackDurationMs = response.item.duration_ms;
-
-                    lyrics = [];
-
-                    let lyricsResponse = await fetch(
-                        "https://spclient.wg.spotify.com/lyrics/v1/track/" + response.item.id, {
-                            "headers": {
+                        $.ajax({
+                            url: `https://spclient.wg.spotify.com/lyrics/v1/track/${lastTrackId}`,
+                            method: "GET",
+                            dataType: "json",
+                            headers: {
+                                "Content-Type": "application/json",
                                 "Authorization": `Bearer ${accessToken}`
                             },
-                            "method": "GET"
-                        }
-                    );
-                    // Used fetch because of less code size
-
-                    if(lyricsResponse.status === 404) {
-                        addLog("Spotify doesn\"t have lyrics for this song.", "warning");
-                        return stop();
-                    }
-                    // If Spotify doesn't have lyrics
-
-                    let lyricsJson = await lyricsResponse.json();
-
-                    for (let line of lyricsJson?.lines) {
-                        lyrics.push({
-                            time: line?.time,
-                            words: line?.words[0]?.string,
+                            async: false,
+                            statusCode: {
+                                200: async (d) => {
+                                    if(d.lines[0].time == undefined) {
+                                        let timePerLyric = Math.round(trackDurationMs / d.lines.length);
+                                        d.lines.reduce((p, c, i, a) => {
+                                            lyrics.push({
+                                                time: p,
+                                                words: c.words[0].string
+                                            });
+                                            return p + timePerLyric;
+                                        }, timePerLyric);
+                                    } else {
+                                        for (let line of d.lines) {
+                                            lyrics.push({
+                                                time: line.time,
+                                                words: line.words[0].string,
+                                            });
+                                        }
+                                    }
+                                },
+                                404: async (x, e) => {
+                                    addLog("Spotify doesn\"t have lyrics for this song.", "error");
+                                    return stop();
+                                }
+                            }
                         });
                     }
+
+                    trackProgressMs = d.progress_ms;
+                },
+                204: async () => {
+                    addLog("You\'re not listening music!", "error");
+                    return stop();
+                },
+                401: async (x, e) => {
+                    refreshAccessToken();
+                    addLog("Refreshed access token due to expired previous one. <strong>Keep working...</strong>", "warning");
+                    return;
+                },
+                404: async (x, e) => {
+                    addLog("Got 404 error, please read <a href=\"https://github.com/OvalQuilter/lyrics-sender#if-something-goes-wrong\">this</a> for more information.", "error");
+                    return;
+                },
+                502: async (x, e) => {
+                    addLog("Got 502 error, might be problem with Spotify itself. If it continues for a time, try refreshing the page or wait few minutes.", "error");
+                    return;
                 }
-
-                trackProgressMs = response.progress_ms;
             }
-        }
-
-        getPlaybackState.send();
+        });
     }, 1000);
     statusChanger = setInterval(function() {
 
@@ -330,6 +364,7 @@ function init() {
 
 
                 changeStatus(`[${formatSeconds((trackProgressMs / 1000).toFixed(0))}] Song lyrics - ${!currentLyric ? "|End|" : currentLyric}`);
+                //                                                                                         Will change some time later
 
 
                 break;
@@ -352,8 +387,5 @@ function stop() {
     stopped = true;
     addLog("Lyrics sender stopped...");
 }
-
-// Lyrics' sender code goes here //
-
 
 $(menuUI).appendTo(document.body);
