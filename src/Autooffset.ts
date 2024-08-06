b1e358e54c746fa13a1f252897a0f44efb2c406e
OvalQuilter <ukotovski@gmail.com>
2024-08-06 17:58:25 +0300

    added autooffset

---
+++ src/Autooffset.ts
@@ -0,0 +1,39 @@
+export class Autooffset {
+    public keys: number[]
+
+    public limit: number
+
+    constructor() {
+        this.keys = []
+
+        this.limit = 0
+    }
+
+    public addValue(value: number): void {
+        console.log(value)
+
+        this.keys.pop()
+
+        this.keys.unshift(value)
+    }
+
+    public getAverageValue(): number {
+        let value = 0
+
+        for (const key of this.keys) {
+            value += key
+        }
+
+        return value / this.keys.length
+    }
+
+    public setLimit(limit: number): void {
+        this.limit = limit
+
+        this.keys.splice(limit)
+
+        for (let i = limit; i > this.keys.length; i--) {
+            this.keys.push(0)
+        }
+    }
+}
