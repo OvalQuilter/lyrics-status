bd51a39b11df43de45cfae2625a6b8a2d497752f
OvalQuilter <ukotovski@gmail.com>
2025-07-23 20:52:56 +0300

    Refactor: Make handler optional

--- src/Bus/MessageDefinition.ts
+++ src/Bus/MessageDefinition.ts
@@ -2,9 +2,9 @@ import { BaseMessageHandler } from "./Handlers/BaseMessageHandler"
 
 export class MessageDefinition<T = unknown> {
     public readonly type: string
-    public readonly handler: BaseMessageHandler<T>
+    public readonly handler: BaseMessageHandler<T> | undefined
 
-    constructor(type: string, handler: BaseMessageHandler<T>) {
+    constructor(type: string, handler?: BaseMessageHandler<T>) {
         this.type = type
         this.handler = handler
     }
