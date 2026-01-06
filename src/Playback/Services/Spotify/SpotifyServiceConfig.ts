f52f2d00ed7205b43c768a57f30fec597a2dd1f0
OvalQuilter <ukotovski@gmail.com>
2026-01-06 17:10:08 +0200

    Style: Remove double quotes from properties

--- src/Playback/Services/Spotify/SpotifyServiceConfig.ts
+++ src/Playback/Services/Spotify/SpotifyServiceConfig.ts
@@ -1,13 +1,13 @@
 import { IServiceConfig } from "../Base/IServiceConfig"
 
 export const SpotifyServiceConfig: IServiceConfig = {
-    "name": "Spotify",
-    "authMethods": [{
-        "name": "OAuth",
-        "type": "oauth",
-        "token": "",
-        "oauthUrl": "",
-        "callbackUrl": "",
+    name: "Spotify",
+    authMethods: [{
+        name: "OAuth",
+        type: "oauth",
+        token: "",
+        oauthUrl: "",
+        callbackUrl: "",
     }],
     isMainService: true,
 }
