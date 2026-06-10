d6299ac571fd06bad39adf86ba49b1264ec59f62
Ramenfighter03 <shizukani33@gmail.com>
2026-06-10 07:25:00 -0400

    fix: add static X-Super-Properties header to discord REST patches

--- dist/StatusChangerBase.js
+++ dist/StatusChangerBase.js
@@ -175,7 +175,7 @@ class StatusChangerBase {
     _discordPatch(body, token) {
         return fetch("https://discord.com/api/v10/users/@me/settings", {
             method: "PATCH",
-            headers: { "Content-Type": "application/json", "Authorization": token || Settings_1.Settings.credentials.token },
+            headers: { "Content-Type": "application/json", "Authorization": token || Settings_1.Settings.credentials.token, "X-Super-Properties": "eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiQ2hyb21lIiwiZGV2aWNlIjoiIiwic3lzdGVtX2xvY2FsZSI6ImVuLVVTIiwiYnJvd3Nlcl91c2VyX2FnZW50IjoiTW96aWxsYS81LjAgKFdpbmRvd3MgTlQgMTAuMDsgV2luNjQ7IHg2NCkgQXBwbGVXZWJLaXQvNTM3LjM2IChLSFRNTCwgbGlrZSBHZWNrbykgQ2hyb21lLzEzNi4wLjAuMCBTYWZhcmkvNTM3LjM2IiwiYnJvd3Nlcl92ZXJzaW9uIjoiMTM2LjAuMC4wIiwib3NfdmVyc2lvbiI6IjEwIiwicmVmZXJyZXIiOiIiLCJyZWZlcnJpbmdfZG9tYWluIjoiIiwicmVmZXJyZXJfY3VycmVudCI6IiIsInJlZmVycmluZ19kb21haW5fY3VycmVudCI6IiIsInJlbGVhc2VfY2hhbm5lbCI6InN0YWJsZSIsImNsaWVudF9idWlsZF9udW1iZXIiOjM5MDAxOCwiY2xpZW50X2V2ZW50X3NvdXJjZSI6bnVsbH0=" },
             body: JSON.stringify(body)
         });
     }
